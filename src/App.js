426b37e9eba69b5b16e4ae8f6edabdd9fdf173cc
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2019-11-20 16:40:39 -0800

    moved action caller for removeAddedFeature to the lower component. Now working

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 import { connect } from 'react-redux';
-import { removeAddedFeature, addFeature } from './actions';
+// import { removeAddedFeature, addFeature } from './actions';
 
 const App = (props) => {
   
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-    removeAddedFeature(item);
-  };
+  // const removeFeature = item => {
+  //   // dispatch an action here to remove an item
+  //   removeAddedFeature(item);
+  // };
 
   // const buyItem = item => {
   //   // dipsatch an action here to add an item
@@ -24,7 +24,9 @@ const App = (props) => {
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} removeFeature={removeFeature} />
+        <AddedFeatures 
+        car={props.car}
+         />
       </div>
       <div className="box">
         <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
@@ -42,4 +44,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { addFeature, removeAddedFeature } )(App);
+export default connect(mapStateToProps, { } )(App);
--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeAddedFeature } from '../actions';
  
 const AddedFeature = props => {
+
+  const handleRemoveFeature = () => {
+    props.removeAddedFeature(props.feature);
+  }
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
       <button 
       className="button"
-      onClick={()=> {
-        props.removeFeature(props.feature)
-      }}
+      onClick={handleRemoveFeature}
       >X</button>
       {props.feature.name}
     </li>
   );
 };
 
+const mapStateToProps = state => {
+  return {
 
-export default AddedFeature;
+  };
+};
+
+export default connect(null, { removeAddedFeature })(AddedFeature);
+// export default AddedFeature;
